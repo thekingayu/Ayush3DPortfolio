1dd71f32d69b1904fece24150c1d6bda0e29124d
Ayush Kumar <ayushraj26877492@gmail.com>
2023-07-17 12:33:24 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        A skilled Software Developer with 1 years of development experience. 
+        I've strong understanding of web technologies, APIs, Database, 
+        Mobile Application development in Flutter. I'm also skilled in Full stack development,
+        AI/ML, Application Development, Linux, Cybersecurity, Computer Networking. 
+        I am a highly motivated and result oriented individual with a strong work ethic. 
+        I am also a team player and I'm always willing to go to extra miles. 
+        I am eager to learn new technologies and improvise my skills.
+        I have some knowledge of market and market trends as I'm a part time Option Trader.
+        If you're looking for a skilled and passionate Software Developer, 
+        i would be honoured to discuss my qualifications with you further.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
